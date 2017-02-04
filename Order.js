fff55a08e87cae319082af070d96fa9373b09f04
jyothsnareddy <S528755@MAIL.NWMISSOURI.EDU>
2017-02-03 22:32:26 -0600

    Add files via upload

---
+++ Order.js
@@ -0,0 +1,34 @@
+
+
+
+
+function function1(){
+
+var answer1=document.getElementById("x").value;
+var answer2=document.getElementById("z").value;
+var ans=parseInt(answer1)+parseInt(answer2);
+result.value=ans;
+event.preventDefault();
+$("#result").css("background-color", "yellow");
+
+var answer3=document.getElementById("y").value;
+var answer4=document.getElementById("z").value;
+var ans1=parseInt(answer3)+parseInt(answer4);
+result.value=ans1;
+event.preventDefault();
+
+var answer5=document.getElementById("x").value;
+var answer6=document.getElementById("a").value;
+var ans2=parseInt(answer5)+parseInt(answer6);
+result.value=ans2;
+event.preventDefault();
+
+var answer7=document.getElementById("s").value;
+var answer8=document.getElementById("a").value;
+var ans3=parseInt(answer7)+parseInt(answer8);
+result.value=ans3;
+event.preventDefault();
+
+
+
+}
